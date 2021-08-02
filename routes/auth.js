525e7dcd6120071007399ba192499f394083be9e
Shanmukh <shanmukh111@gmail.com>
2021-08-02 11:34:33 +0530

    Added auth router

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const db = require('../models');
+
+router.post('/login', async (req, res, next) => {
+
+})
+
+router.post('/register', async (req, res, next) => {
+
+})
+
+module.exports = router;
