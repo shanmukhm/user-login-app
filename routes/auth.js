a94a9c8e0f1744b418340d4bbe23685b75470482
Shanmukh <shanmukh111@gmail.com>
2021-08-02 11:49:45 +0530

    Added endpoint to register user

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,51 @@
 const router = require('express').Router();
 const db = require('../models');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const { user } = require('../models');
+
+dotenv.config();
 
 router.post('/login', async (req, res, next) => {
 
 })
 
 router.post('/register', async (req, res, next) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+        if (!(email && password && firstName && lastName)) {
+            res.status(400).send("All input is required");
+        }
+        const oldUser = await db.user.findOne({ email });
+        if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
+        }
+        const encryptedPassword = await bcrypt.hash(password, 10);
 
+        const userCreated = await db.user.create({
+            firstName,
+            lastName,
+            email: email.toLowerCase(),
+            password: encryptedPassword
+        });
+        console.log(process.env.TOKEN_SECRET);
+        const token = jwt.sign(
+            {
+                userId: userCreated.id,
+                email
+            }, process.env.TOKEN_SECRET,
+            {
+                expiresIn: "2h",
+            }
+        );
+        userCreated.token = token;
+
+        res.status(201).json(userCreated);
+    } catch (error) {
+        console.log(`Error while registration`, error);
+        res.status(500).send(`Error occured`);
+    }
 })
 
 module.exports = router;
