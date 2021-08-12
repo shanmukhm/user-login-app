6856fd509cde634b74533b32060dd0c3d5699189
Shanmukh <shanmukh111@gmail.com>
2021-08-12 18:40:42 +0530

    Added a sample test case

---
+++ tests/api/users/users.test.js
@@ -0,0 +1,9 @@
+// const request = require('supertest');
+
+// const app = require('../../../app');
+
+describe('Initial Test', () => {
+    it('should test that 1 + 1 === 2', () => {
+      expect(1+1).toBe(2)
+    })
+  })
