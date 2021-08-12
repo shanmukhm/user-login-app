f7fbddb4772e4b2bbc15884072816864a42c84c4
Shanmukh <shanmukh111@gmail.com>
2021-08-12 21:17:39 +0530

    Added scripts for testing

--- config/config.js
+++ config/config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    TOKEN_SECRET: 'd9833fb8b3c76ec8b43fdcd126ff101fc47b88bcbc454396ed7146a10f997ef9f0bf2405aa6d6636be51972aa74b6f7db16a29046b490b00bf60e65f2274cede'
+    TOKEN_SECRET: 'd9833fb8b3c76ec8b43fdcd126ff101fc47b88bcbc454396ed7146a10f997ef9f0bf2405aa6d6636be51972aa74b6f7db16a29046b490b00bf60e65f2274cede',
+    "test": {
+        "username": "sbuser",
+        "password": "sbpwd",
+        "db": "sugarboxdb-test",
+        "dialect": "mysql",
+        "host": "localhost"
+    }
 }
